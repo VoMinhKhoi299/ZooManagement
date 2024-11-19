42872997c3af3b3691fd2c1883b81454afcd0490
Azure <nguyenoanhtld@gmail.com>
2024-11-19 13:58:53 +0700

    "Việc kết hợp cả hai cách (sử dụng hàm Sort() và tự viết thuật toán sắp xếp)"
    "Việc kết hợp cả hai cách (sử dụng hàm Sort() và tự viết thuật toán sắp xếp)"

--- SortFunction.cs
+++ SortFunction.cs
@@ -1,85 +1,105 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace CK
 {
-	public static class SortFunction
-	{
-        
-            // Sắp xếp động vật theo tên (tăng dần hoặc giảm dần)
-            public static void SortAnimalsByName(List<Animal> animals, bool ascending = true)
+    public static class SortFunction
+    {
+        // Sắp xếp động vật theo tên
+        public static void SortAnimalsByName(List<Animal> animals, bool ascending = true)
+        {
+            if (animals.Count > 100) // Sử dụng Sort() cho danh sách lớn
             {
-                if (ascending)
+                animals.Sort((a, b) =>
                 {
-                    animals.Sort((a, b) => string.Compare(a.name, b.name, StringComparison.Ordinal));
-                }
-                else
+                    return ascending
+                        ? string.Compare(a.name, b.name, StringComparison.Ordinal)
+                        : string.Compare(b.name, a.name, StringComparison.Ordinal);
+                });
+            }
+            else // Dùng Bubble Sort cho danh sách nhỏ
+            {
+                int n = animals.Count;
+                for (int i = 0; i < n - 1; i++)
                 {
-                    animals.Sort((a, b) => string.Compare(b.name, a.name, StringComparison.Ordinal));
+                    for (int j = 0; j < n - i - 1; j++)
+                    {
+                        bool condition = ascending
+                            ? string.Compare(animals[j].name, animals[j + 1].name, StringComparison.Ordinal) > 0
+                            : string.Compare(animals[j].name, animals[j + 1].name, StringComparison.Ordinal) < 0;
+
+                        if (condition)
+                        {
+                            var temp = animals[j];
+                            animals[j] = animals[j + 1];
+                            animals[j + 1] = temp;
+                        }
+                    }
                 }
             }
+        }
 
-            // Sắp xếp động vật theo cân nặng (tăng dần hoặc giảm dần)
-            public static void SortAnimalsByWeight(List<Animal> animals, bool ascending = true)
+        // Sắp xếp động vật theo cân nặng
+        public static void SortAnimalsByWeight(List<Animal> animals, bool ascending = true)
+        {
+            if (animals.Count > 100) // Sử dụng Sort() cho danh sách lớn
             {
-                if (ascending)
+                animals.Sort((a, b) =>
                 {
-                    animals.Sort((a, b) => a.weight.CompareTo(b.weight));
-                }
-                else
+                    return ascending
+                        ? a.weight.CompareTo(b.weight)
+                        : b.weight.CompareTo(a.weight);
+                });
+            }
+            else // Dùng Selection Sort cho danh sách nhỏ
+            {
+                int n = animals.Count;
+                for (int i = 0; i < n - 1; i++)
                 {
-                    animals.Sort((a, b) => b.weight.CompareTo(a.weight));
+                    int selected = i;
+                    for (int j = i + 1; j < n; j++)
+                    {
+                        bool condition = ascending
+                            ? animals[j].weight < animals[selected].eight
+                            : animals[j].weight > animals[selected].weight;
+
+                        if (condition)
+                        {
+                            selected = j;
+                        }
+                    }
+
+                    var temp = animals[i];
+                    animals[i] = animals[selected];
+                    animals[selected] = temp;
                 }
             }
+        }
 
-            // Sắp xếp động vật theo ID (tăng dần)
-            public static void SortAnimalsByID(List<Animal> animals)
+        // Sắp xếp động vật theo ID
+        public static void SortAnimalsByID(List<Animal> animals)
+        {
+            if (animals.Count > 100) // Sử dụng Sort() cho danh sách lớn
             {
                 animals.Sort((a, b) => string.Compare(a.ID, b.ID, StringComparison.Ordinal));
             }
-        
-
- 
-		
-
-            // Sắp xếp chuồng theo loại (tăng dần hoặc giảm dần)
-            public static void SortCagesByType(List<Cage> cages, bool ascending = true)
+            else // Dùng Insertion Sort cho danh sách nhỏ
             {
-                if (ascending)
+                int n = animals.Count;
+                for (int i = 1; i < n; i++)
                 {
-                    cages.Sort((c1, c2) => string.Compare(c1.Type, c2.Type, StringComparison.Ordinal));
-                }
-                else
-                {
-                    cages.Sort((c1, c2) => string.Compare(c2.Type, c1.Type, StringComparison.Ordinal));
+                    var key = animals[i];
+                    int j = i - 1;
+
+                    while (j >= 0 && string.Compare(animals[j].ID, key.ID, StringComparison.Ordinal) > 0)
+                    {
+                        animals[j + 1] = animals[j];
+                        j--;
+                    }
+
+                    animals[j + 1] = key;
                 }
             }
-
-            // Sắp xếp chuồng theo số lượng động vật bên trong (tăng dần hoặc giảm dần)
-            public static void SortCagesByAnimalCount(List<Cage> cages, bool ascending = true)
-            {
-                if (ascending)
-                {
-                    cages.Sort((c1, c2) => c1.CurrentAnimals.Count.CompareTo(c2.CurrentAnimals.Count));
-                }
-                else
-                {
-                    cages.Sort((c1, c2) => c2.CurrentAnimals.Count.CompareTo(c1.CurrentAnimals.Count));
-                }
-            }
-
-            // Sắp xếp chuồng theo diện tích (tăng dần hoặc giảm dần)
-            public static void SortCagesBySize(List<Cage> cages, bool ascending = true)
-            {
-                if (ascending)
-                {
-                    cages.Sort((c1, c2) => c1.size.CompareTo(c2.size));
-                }
-                else
-                {
-                    cages.Sort((c1, c2) => c2.size.CompareTo(c1.size));
-                }
-            }
-        
-
+        }
     }
 }
-
